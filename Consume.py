fbd3ad608bca22619a83ff1f318b7d439e5280a1
Nirmal Choudhary <nirmalchoudhary779@gmail.com>
2024-06-09 14:00:15 +0530

    Update Consume.py

--- Consume.py
+++ Consume.py
@@ -35,7 +35,8 @@ body = str.encode(json.dumps(data))
 
 url = 'https://crop-recommendation-proje-lkwci.eastus2.inference.ml.azure.com/score'
 # Replace this with the primary/secondary key, AMLToken, or Microsoft Entra ID token for the endpoint
-api_key = ''
+#Caution : this api key has expired
+api_key = 'ntdwBMYaujqUtbigQda2RbLlDhkbejxB'
 if not api_key:
     raise Exception("A key should be provided to invoke the endpoint")
 
